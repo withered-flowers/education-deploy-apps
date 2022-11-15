e29eda7c8a6e1b6784a8d7863f28f791bacf4977
Withered Flowers <withered-flowers@users.noreply.github.com>
2022-11-15 00:28:42 +0000

    feat: change code Vue v3 to use Provide / Inject; change content readme.md to describe to step to use Vue v3

--- src/frontend-vue3/src/main.js
+++ src/frontend-vue3/src/main.js
@@ -3,4 +3,11 @@ import App from "./App.vue";
 
 import "./assets/base.css";
 
-createApp(App).mount("#app");
+// TODO: change this code to provide global variable
+// createApp(App).mount("#app");
+
+// TODO: Change baseUrl to the project on cloud provider
+// Railway = https://xxxx.up.railway.app
+const app = createApp(App);
+app.provide("baseUrl", "http://localhost:10000");
+app.mount("#app");
